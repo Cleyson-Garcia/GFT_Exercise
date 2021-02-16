7f1a21359e31517bcfa18e641ae384a80137299c
Cleyson-Garcia <45490430+Cleyson-Garcia@users.noreply.github.com>
2021-02-15 22:41:45 -0300

    First Exercises

--- src/main/java/br/dio/exercicios/Practice.java
+++ src/main/java/br/dio/exercicios/Practice.java
@@ -1,7 +1,20 @@
 package br.dio.exercicios;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello\nCleyson Garcia!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a number: ");
+        Double number = in.nextDouble();
+        char[] individuals = number.toString().toCharArray();
+
+        for(char value:individuals){
+            System.out.print(value + " ");
+        }
     }
 }
